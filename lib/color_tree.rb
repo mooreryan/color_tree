d48779f1d6da10325264ebd7ee8b5a8f28650e6d
Ryan Moore <moorer@udel.edu>
2016-03-22 18:40:29 -0400

    Organize

---
+++ lib/color_tree.rb
@@ -0,0 +1,2 @@
+require_relative "const/const"
+require_relative "utils/utils"
---
+++ lib/const/const.rb
@@ -0,0 +1,13 @@
+module ColorTree
+  module Const
+    VERSION = "0.5"
+    COPYRIGHT = "2015, 2016 Ryan Moore"
+    CONTACT = "moorer@udel.edu"
+    LICENSE = "GPLv3"
+
+    VERSION_BANNER = "  # Version: #{VERSION}
+  # Copyright #{COPYRIGHT}
+  # Contact: #{CONTACT}
+  # License: #{LICENSE}"
+  end
+end
